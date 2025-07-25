ca72206e844ed86600385b0c4de631aecd2d587e
Lakshay Khurana <lakshay@example.com>
2025-07-25 17:44:44 +0530

    Added back to home link on EV bikes page

--- FF.py
+++
@@ -1,7 +0,0 @@
-def AB(a,b):
-    D= a+b
-    return D
-a =3
-b=4
-print(AB(a,b))
-
--- Game/Game/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home_view(request):
-    return render(request, 'index.html')
-
-def ev_bikes_view(request):
-    return render(request, 'ev_bikes.html')  # use underscore
